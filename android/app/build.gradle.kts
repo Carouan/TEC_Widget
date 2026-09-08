b8a6581477d1ac70c01cc9906b6dcb4d69beb726
Carouan <baudoux.sebastien@gmail.com>
2026-09-08 06:48:08 +0200

    chore(android): configure app module

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "be.carouan.tecwidget"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "be.carouan.tecwidget"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("com.google.androidbrowserhelper:androidbrowserhelper:2.6.2")
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
+}
